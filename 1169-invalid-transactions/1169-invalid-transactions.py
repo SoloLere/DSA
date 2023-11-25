522bf2b2048ff9be793ec3872ed8581fce9cf601
Oseni Solomon <74763594+SoloLere@users.noreply.github.com>
2023-11-25 16:33:33 +0100

    Time: 61 ms (92.97%), Space: 17.2 MB (24.28%) - LeetHub

--- 1169-invalid-transactions/1169-invalid-transactions.py
+++ 1169-invalid-transactions/1169-invalid-transactions.py
@@ -1,27 +1,42 @@
 class Solution:
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        names, times, amounts, citys, res = [], [], [], [], [False for i in range(len(transactions))];
+        hashmap = {}
+        res = []
         
         for i in range(len(transactions)):
             name, time, amount, city = transactions[i].split(',')
-            names.append(name);
-            times.append(int(time));
-            amounts.append(int(amount));
-            citys.append(city);
+            time = int(time)
+            amount = int(amount)
+            if time not in hashmap:
+                hashmap[time] = {name: set([city])}
+            else:
+                if name not in hashmap[time]:
+                    hashmap[time][name] = set([city])
+                else:
+                    hashmap[time][name].add(city)
+                    
+        for transaction in transactions:
+            name, time, amount, city = transaction.split(',')
+            time = int(time)
+            amount = int(amount)
+            
+            if amount > 1000:
+                res.append(transaction)
+                
+            else:
+                for time in range(time - 60, time + 61):
+                    if time not in hashmap:
+                        continue
+                    if name not in hashmap[time]:
+                        continue
+                    if len(hashmap[time][name]) > 1 or next(iter(hashmap[time][name])) != city:
+                        res.append(transaction)
+                        break
+        
+        return res
+                        
+            
+            
             
         
-        for i in range(len(transactions)):
-            if amounts[i] > 1000:
-                res[i] = True
-                                
-            for j in range(i + 1, len(transactions)):
-                if names[i] == names[j] and citys[i] != citys[j] and abs(times[i] - times[j]) <= 60:
-                    res[i] = True
-                    res[j] = True
-                    
-               
-        ans = []
-        for i in range(len(res)):
-            if res[i] == True:
-                ans.append(transactions[i])
-        return ans
+        
